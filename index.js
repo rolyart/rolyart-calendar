ebc1fd4e33e726e1c7c32b41f9a223f3358185ba
Roland Maruntalu <rolandmaruntelu@gmail.com>
2019-09-14 06:34:19 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let calendarConfig = {
+    container: 'calendar',
+    months: ["January", "February", "March", "Aprile", "May", "June", "July", "August", "September", "Octomber", "November", "December"],
+    weekDays: ["S", "M", "T", "W", "T", "F", "S"],
+}
+const calendar = new RolyartCalendar(calendarConfig);
---
+++ rolyart-calendar.js
@@ -0,0 +1,178 @@
+function RolyartCalendar(config){
+    this.container = document.getElementById(config.container);
+    this.container.classList.add('rolyart-calendar');
+    this.today = new Date();
+    this.selected  = this.today;
+    this.currentMonth = this.today.getMonth();
+    this.currentYear = this.today.getFullYear();
+    this.months = config.months;
+    this.weekDays = config.weekDays;
+    /** 
+     * Calendar navigation
+     * nextMonth()
+     * prevMonth()
+     */
+    this.nextMonth = ()=>{
+        if ( this.currentMonth == 11 ) {
+            this.currentMonth = 0;
+            this.currentYear = this.currentYear + 1;
+        }
+        else {
+            this.currentMonth = this.currentMonth + 1;
+        }
+        this.showCalendar(this.currentYear, this.currentYear);
+    }
+    this.prevMonth = ()=>{
+        if ( this.currentMonth == 0 ) {
+            this.currentMonth = 11;
+            this.currentYear = this.currentYear - 1;
+        }
+        else {
+            this.currentMonth = this.currentMonth - 1;
+        }
+        this.showCalendar(this.currentYear, this.currentYear);
+    }
+    /** 
+     * Get days of month
+     * getPrevDays()
+     * getNextDays()
+     * getCurrentDays()
+     */
+    this.getPrevDays = (date, staDay=0)=>{
+        let ret = [];
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        let firstWeekday =  new Date(year, month, 1).getDay();
+        let days = (firstWeekday + 7) - (staDay +7) - 1;
+        for (let i=days * -1; i<=0;i++){
+            ret.push({date:new Date(year, month, i).getDate(), type:"not-current", id:new Date(year, month, i) });  
+        }
+        return ret;
+    }
+    this.getNextDays = (prevMonthDays, monthDays)=>{
+        let ret = [];
+        let days = 42 - (prevMonthDays.length + monthDays.length);
+        for(let i = 1; i<=days; i++){
+            ret.push({date:i, type:"not-current"});
+        }
+        return ret;
+    }
+    this.getCurrentDays = (date)=>{
+        let ret = [];
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        let lastDay = new Date(year, month +1 , 0).getDate();
+        for(let i = 1; i<=lastDay;i++){
+            ret.push({date:i, type:"current", id:this.YYYYmmdd(new Date(year, month, i)) });
+        }
+        return ret;
+    }
+
+    /** 
+     * YYYY-MM-DD date format
+     * YYYYmmdd()
+     */
+    this.YYYYmmdd = (date)=>{
+        let d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+        day = '0' + day;
+        return [year, month, day].join('-');
+    }
+
+    this.calendarHeader = ()=>{
+        let header = document.createElement('header');
+        header.classList.add('calendar-header');
+        let monthAndYear = document.createElement('h3');
+        let prevMonth = document.createElement('button');
+        let currentMonth =  document.createElement('button');
+        let nextMonth = document.createElement('button');
+
+        monthAndYear.classList.add('month-year');
+        monthAndYear.innerHTML = `${this.months[this.currentMonth] +' '+ this.currentYear}`;
+        
+        prevMonth.innerHTML = '<i class="arrow prev-month"></i>'
+        prevMonth.addEventListener('click', ()=>{
+            this.prevMonth(); 
+            monthAndYear.innerHTML = `${this.months[this.currentMonth] +' '+ this.currentYear}`;
+        })
+
+        nextMonth.innerHTML = '<i class="arrow next-month"></i>'
+        nextMonth.addEventListener('click', ()=>{
+            this.nextMonth(); 
+            monthAndYear.innerHTML = `${this.months[this.currentMonth] +' '+ this.currentYear}`;
+        })
+
+
+        currentMonth.innerHTML = '<i class="current-month"></i>'
+        currentMonth.addEventListener('click', ()=>{
+            this.currentYear = new Date().getFullYear();
+            this.currentMonth = new Date().getMonth();
+            monthAndYear.innerHTML = `${this.months[this.currentMonth] +' '+ this.currentYear}`;
+            this.showCalendar();
+        })
+
+        let weekDays = document.createElement('div');
+        weekDays.classList.add('week-days');
+        for(let i = 0; i<=6;i++){
+            weekDays.innerHTML +=`<div>${this.weekDays[i]}</div>`;
+        }
+
+        header.appendChild(monthAndYear)
+        header.appendChild(prevMonth);
+        header.appendChild(currentMonth);
+        header.appendChild(nextMonth);
+        this.container.appendChild(header);
+        this.container.appendChild(weekDays);
+    }
+    
+    this.calendarBody = (year, month)=>{
+        year = this.currentYear;
+        month = this.currentMonth;
+        let date = new Date(year, month+1, 0);
+        let daysPrevMonth = this.getPrevDays(date);
+        let daysThisMonth = this.getCurrentDays(date);
+        let daysNextMonth = this.getNextDays(daysPrevMonth, daysThisMonth);
+        let calendarBody = document.createElement('div');
+        calendarBody.classList.add('calendar-body');
+        [...daysPrevMonth, ...daysThisMonth, ...daysNextMonth]
+        .forEach(num=>{
+            let cell = document.createElement('div');
+            cell.setAttribute('id', num.id);
+            cell.classList.add('day');
+            let day = document.createElement('span');
+            
+            day.innerHTML = num.date;
+            cell.appendChild(day);
+            cell.addEventListener('click', ()=>{
+                this.selected = num.id;
+                
+                let selected = document.getElementsByClassName("selected");
+                if (selected.length > 0) { 
+                    selected[0].className = selected[0].className.replace(" selected", "");
+                }         
+                cell.className += " selected"; 
+            });
+            num.type === 'not-current'?cell.classList.add('not-current'):cell.classList.add('current');
+            if(num.id === this.YYYYmmdd(this.today)){
+                cell.classList.add('active');
+            }
+            calendarBody.appendChild(cell);
+        })
+        this.container.appendChild(calendarBody);
+    }
+
+    this.showCalendar = (year, month)=>{
+        this.container.innerHTML = '';
+        this.calendarHeader();
+        this.calendarBody(year, month);
+    }
+
+    this.showCalendar(this.currentYear, this.currentMonth);
+}
+
+
