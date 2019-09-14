655f9178a72399390c048c66a7a405cb28281a8f
Roland Maruntalu <rolandmaruntelu@gmail.com>
2019-09-14 06:36:55 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-let calendarConfig = {
-    container: 'calendar',
-    months: ["January", "February", "March", "Aprile", "May", "June", "July", "August", "September", "Octomber", "November", "December"],
-    weekDays: ["S", "M", "T", "W", "T", "F", "S"],
-}
-const calendar = new RolyartCalendar(calendarConfig);
